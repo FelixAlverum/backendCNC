e33d6ad9e1308afef59dffa1dbfb9d2ea1244f5c
Felix Fahrbach <Felix.Fahrbach@gmail.com>
2022-11-13 01:26:53 +0100

    Speichern Beginn mit SVG verstehen

--- src/cnc/cnc_picture/LogicPicture.java
+++ src/cnc/cnc_picture/LogicPicture.java
@@ -3,6 +3,8 @@ package cnc.cnc_picture;
 public class LogicPicture {
     /**
      * NUR SVG erlauben
+     * https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes
+     * https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Paths
      */
 
 }
