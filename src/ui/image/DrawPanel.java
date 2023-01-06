ab5b88870e72b4074e688cfc2a3c9afdfc731185
Felix Fahrbach <Felix.Fahrbach@gmail.com>
2023-01-06 01:24:18 +0100

    UI fix

--- src/ui/image/DrawPanel.java
+++ src/ui/image/DrawPanel.java
@@ -3,135 +3,152 @@ package ui.image;
 import ui.res.UI_CONST;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
  * https://wiki.byte-welt.net/wiki/Malen_in_Swing_Teil_2:_ein_einfaches_Malprogramm
+ * https://stackoverflow.com/questions/5621338/how-to-add-jtable-in-jpanel-with-null-layout/5630271#5630271
  */
 public class DrawPanel extends JPanel {
 
-    private JPanel mainPanel, drawPanel, brushPanel, footerPanel;
+    private JPanel drawPanel, brushPanel, footerPanel;
+    GridBagConstraints gbc;
 
-    public DrawPanel(){
+    public DrawPanel() {
+        setLayout(new GridBagLayout());
+        gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.BOTH;
+
         initDrawPanel();
         initBrushPanel();
-        //initFooterPanel();
-
-        // mainPanel = new JPanel();
-
-        //GridBagLayout gridBagLayout = new GridBagLayout();
-        //mainPanel.setLayout(gridBagLayout);
-        //GridBagConstraints c = new GridBagConstraints();
-
-//        c.fill = GridBagConstraints.HORIZONTAL;
-//        c.weightx = 0.5;
-//        c.gridx = 0;
-//        c.gridy = 0;
-//        c.gridwidth = 1;
-//        c.gridheight = 1;
-//        gridBagLayout.setConstraints(drawPanel, c);
-//        mainPanel.add(drawPanel);
-
-//        c.fill = GridBagConstraints.HORIZONTAL;
-//        c.weightx = 0.5;
-//        c.gridx = 1;
-//        c.gridy = 0;
-//        c.gridwidth = 1;
-//        c.gridheight = 1;
-//        gridBagLayout.setConstraints(brushPanel, c);
-//        mainPanel.add(brushPanel);
-
-//        c.fill = GridBagConstraints.HORIZONTAL;
-//        c.gridx = 0;
-//        c.gridy = 1;
-//        c.gridwidth = 2;
-//        c.gridheight = 1;
-//        gridBagLayout.setConstraints(footerPanel, c);
-//        mainPanel.add(footerPanel);
-
-        mainPanel = new JPanel(new GridLayout(5, 1));
-        mainPanel.add(drawPanel);
-        mainPanel.add(brushPanel);
-        //mainPanel.add(footerPanel);
+        initFooterPanel();
     }
 
-    private void initDrawPanel(){
-        drawPanel = new JPanel();
-        setBackground(Color.CYAN);
+    private void initDrawPanel() {
+        drawPanel = new JPanel(new BorderLayout());
+        drawPanel.setBorder(new TitledBorder("Drawing Panel"));
+        drawPanel.setBackground(Color.WHITE);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridheight = 7;
+        gbc.gridwidth = 8;
+        gbc.weightx =0.8;
+        gbc.weighty =0.9;
+        add(drawPanel, gbc);
     }
 
-    private void initBrushPanel(){
-        brushPanel = new JPanel();
-        setBackground(Color.DARK_GRAY);
+    private void initBrushPanel() {
+        brushPanel = new JPanel(new GridLayout(0, 1, 5, 5));
+        brushPanel.setBorder(new TitledBorder("Brush Panel"));
+        brushPanel.setBackground(Color.LIGHT_GRAY);
+
+        gbc.gridx = 8;
+        gbc.gridy = 0;
+        gbc.gridheight = 7;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.weightx =0.2;
+        gbc.weighty =0.9;
+        add(brushPanel, gbc);
+
     }
 
-    private void initFooterPanel(){
-        footerPanel = new JPanel();
-        setBackground(Color.GREEN);
+    private void initFooterPanel() {
+        footerPanel = new JPanel(new BorderLayout());
+        footerPanel.setBackground(Color.GRAY);
+        footerPanel.setBorder(new LineBorder(Color.GRAY, 5));
+
+        JButton back = new JButton(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                UI_CONST.cardLayout.show(UI_CONST.showPanel, UI_CONST.MAIN_PANEL);
+            }
+        });
+        back.setText("Back");
+        footerPanel.add(back, BorderLayout.LINE_START);
+
+        JButton next = new JButton(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // #TODO auf folgepage navigieren
+                UI_CONST.cardLayout.show(UI_CONST.showPanel, UI_CONST.MAIN_PANEL);
+            }
+        });
+        next.setText("Next");
+        footerPanel.add(next, BorderLayout.LINE_END);
+
+        gbc.gridx = 0;
+        gbc.gridy = 7;
+        gbc.gridheight = GridBagConstraints.REMAINDER;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.weightx =1.0;
+        gbc.weighty =0.1;
+        add(footerPanel, gbc);
     }
 
     /**
-    final private DrawingPanel drawingPanel;
-    final private JPanel buttonPanel;
-    final private JButton clearButton, upSize, downSize;
-    private final ActionListener actionHandler;
+     final private DrawingPanel drawingPanel;
+     final private JPanel buttonPanel;
+     final private JButton clearButton, upSize, downSize;
+     private final ActionListener actionHandler;
 
 
-    public DrawPanel(){
-        JFrame frame = new JFrame("DrawOnImage");
-        drawingPanel = new DrawingPanel();
-        actionHandler = new ActionListener() {
+     public DrawPanel(){
+     JFrame frame = new JFrame("DrawOnImage");
+     drawingPanel = new DrawingPanel();
+     actionHandler = new ActionListener() {
 
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                String s = e.getActionCommand();
-                if (s.equals("Paint")) {
-                    JButton button = (JButton) e.getSource();
-                    drawingPanel.setPaintColor(button.getBackground());
-                } else if (s.equals("Clear")) {
-                    drawingPanel.clearPaint();
-                } else if (s.equals("+")) {
-                    drawingPanel.increaseBrushSize();
-                } else {
-                    drawingPanel.decreaseBrushSize();
-                }
-            }
-        };
-        buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(2, 0, 2, 2));
-        addButton(Color.BLACK);
-        addButton(Color.BLUE);
-        addButton(Color.GREEN);
-        upSize = addButton(null);
-        upSize.setText("+");
-        addButton(Color.RED);
-        addButton(Color.ORANGE);
-        clearButton = addButton(null);
-        clearButton.setText("Clear");
-        downSize = addButton(null);
-        downSize.setText("-");
-        frame.getContentPane().add(new JScrollPane(drawingPanel));
-        frame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+    @Override public void actionPerformed(final ActionEvent e) {
+    String s = e.getActionCommand();
+    if (s.equals("Paint")) {
+    JButton button = (JButton) e.getSource();
+    drawingPanel.setPaintColor(button.getBackground());
+    } else if (s.equals("Clear")) {
+    drawingPanel.clearPaint();
+    } else if (s.equals("+")) {
+    drawingPanel.increaseBrushSize();
+    } else {
+    drawingPanel.decreaseBrushSize();
     }
-
-    private JButton addButton(final Color color) {
-        JButton button = new JButton();
-        button.setBackground(new Color(230, 240, 250));
-        button.setBorder(BorderFactory.createEtchedBorder());
-        if (color != null) {
-            button.setForeground(Color.WHITE);
-            button.setBackground(color);
-        }
-        button.setText("Paint");
-        buttonPanel.add(button);
-        button.addActionListener(actionHandler);
-        return (button);
     }
-    */
+    };
+     buttonPanel = new JPanel();
+     buttonPanel.setLayout(new GridLayout(2, 0, 2, 2));
+     addButton(Color.BLACK);
+     addButton(Color.BLUE);
+     addButton(Color.GREEN);
+     upSize = addButton(null);
+     upSize.setText("+");
+     addButton(Color.RED);
+     addButton(Color.ORANGE);
+     clearButton = addButton(null);
+     clearButton.setText("Clear");
+     downSize = addButton(null);
+     downSize.setText("-");
+     frame.getContentPane().add(new JScrollPane(drawingPanel));
+     frame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+     frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+     frame.pack();
+     frame.setLocationRelativeTo(null);
+     frame.setVisible(true);
+     }
+
+     private JButton addButton(final Color color) {
+     JButton button = new JButton();
+     button.setBackground(new Color(230, 240, 250));
+     button.setBorder(BorderFactory.createEtchedBorder());
+     if (color != null) {
+     button.setForeground(Color.WHITE);
+     button.setBackground(color);
+     }
+     button.setText("Paint");
+     buttonPanel.add(button);
+     button.addActionListener(actionHandler);
+     return (button);
+     }
+     */
 }
