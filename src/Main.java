cf305b9281b6544f0df86faad332feedd292e27c
FahrbachFel <Felix.Fahrbach@gmail.com>
2023-01-16 11:14:37 +0100

    sync

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 import cnc.CncState;
+import cnc.cnc_instructions.Parser;
 import com.fazecast.jSerialComm.SerialPort;
 import connection.cnc.SerialAPI;
+
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello CNC Backend");
+
+        Parser p = new Parser();
+        p.splitSvgCode(null);
 
     }
 }
--- src/cnc/cnc_instructions/Parser.java
+++ src/cnc/cnc_instructions/Parser.java
@@ -1,6 +1,12 @@
 package cnc.cnc_instructions;
 
+import org.apache.batik.dom.svg.SAXSVGDocumentFactory;
+import org.apache.batik.util.XMLResourceDescriptor;
+import org.w3c.dom.svg.SVGDocument;
+
 import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,9 +18,17 @@ public class Parser {
     private ArrayList<String> gCode = new ArrayList<String>();
     private ArrayList<String> svg = new ArrayList<String>();
 
-    public void splitSvgCode(File svg){
+    public void splitSvgCode(File svg) throws IOException {
         // get the SVG as String --> batik jar bekommen
         // https://stackoverflow.com/questions/26027313/how-to-load-and-parse-svg-documents
+
+        StringReader reader = new StringReader("src/Test/DrawPanelSVG.svg");
+        String uri = "file:src/Test/DrawPanelSVG.svg";
+        String parser = XMLResourceDescriptor.getXMLParserClassName();
+        SAXSVGDocumentFactory f = new SAXSVGDocumentFactory(parser);
+        SVGDocument doc = f.createSVGDocument(uri, reader);
+
+        System.out.println(doc);
     }
 
     public void resizeCoordinates(File svg){
