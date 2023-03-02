f5fa1225ec1ddd51e543e4d94ed3bde334d479c5
FahrbachFel <Felix.Fahrbach@gmail.com>
2023-03-02 12:39:13 +0100

    GCode koordinaten werden erstellt

--- src/cnc/cnc_instructions/Parser.java
+++ src/cnc/cnc_instructions/Parser.java
@@ -13,7 +13,7 @@ public class Parser {
      * GCode in den VHS CNC Code übersetzen
      */
 
-    private ArrayList<String> gCode = new ArrayList<String>();
+    private ArrayList<int[]> gCode = new ArrayList<int[]>();
     private ArrayList<String[]> svg = new ArrayList<String[]>();
 
     public void splitSvgCode() throws IOException {
@@ -74,7 +74,7 @@ public class Parser {
 
             System.out.println("pxCmScale " + pxCmScale);
 
-            if (pxCmScale > ((double) CncState.workpart_length / (double)CncState.canvas_length) ){
+            if (pxCmScale > ((double) CncState.workpart_length / (double) CncState.canvas_length)) {
                 pxCmScale = CncState.workpart_length / CncState.canvas_length;
             }
 
@@ -88,22 +88,20 @@ public class Parser {
             double new_w = pxCmScale * (CncState.canvas_width / gcf);
             double new_l = pxCmScale * (CncState.canvas_length / gcf);
 
-            System.out.println("neue länge: " + pxCmScale + " /(" + CncState.canvas_length +"/"+ gcf +") = "+ new_w);
-            System.out.println("neue länge: " + pxCmScale + " /(" + CncState.canvas_width +"/"+ gcf +") = "+ new_l);
+            System.out.println("neue länge: " + pxCmScale + " /(" + CncState.canvas_length + "/" + gcf + ") = " + new_w);
+            System.out.println("neue länge: " + pxCmScale + " /(" + CncState.canvas_width + "/" + gcf + ") = " + new_l);
 
             // calculate offset
-            double offsetX = ((double)CncState.workpart_width - new_w) / 2;
-            double offsetY = ((double)CncState.workpart_length - new_l) / 2;
+            double offsetX = ((double) CncState.workpart_width - new_w) / 2;
+            double offsetY = ((double) CncState.workpart_length - new_l) / 2;
 
             System.out.println("offsetX: " + offsetX);
             System.out.println("offsetY: " + offsetY);
 
-            double millimeterPixelRatio = new_w / (double)CncState.canvas_length;
+            double millimeterPixelRatio = new_w / (double) CncState.canvas_length;
             System.out.println("millimeterPixelRatio: " + millimeterPixelRatio);
 
-            String newSvg = "<svg width=\""+CncState.workpart_width+"mm\" height=\""+CncState.workpart_length+"mm\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n";
-
-            int qCase = 0;
+            String newSvg = "<svg width=\"" + CncState.workpart_width + "mm\" height=\"" + CncState.workpart_length + "mm\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n";
 
             for (String[] path : this.svg) {
                 newSvg += "<path fill=\"white\" stroke=\"red\" stroke-width=\"2mm\" opacity=\"1.0\"\n d=\"";
@@ -111,7 +109,7 @@ public class Parser {
                     if (s.equals("") || s == null) {
                         continue;
                     }
-                    if(s.equals("L")){
+                    if (s.equals("L")) {
                         newSvg += " L ";
                     } else if (s.equals("Q")) {
                         newSvg += " L "; // TODO bei Q nur die nächsten 2 Werte übernehmen und nicht alle folgenden
@@ -128,13 +126,94 @@ public class Parser {
             newSvg += "</svg>";
 
 
+            // G-Code Koordinaten kalkulieren für goCoordinate Befehle
+            for (int path = 0; path < svg.size(); path++) {
+                svg.get(path);
+                int i = 0;
+                while (i < svg.get(path).length) {
+                    String s = svg.get(path)[i];
+
+                    // Mache nichs wenn leeres Feld
+                    if (s.equals("") || s == null) {
+                        continue;
+                    }
+
+                    // -1 = Nehme den Wert aus Absolute (CncState)
+                    // -2 = Nehme den Wert aus Absolute_Z und rechne ein Offset von 1cm nach oben (CncState)
+                    // -3 = Nehme den Wert aus Absolute_Z und rechne ein Offset von 1cm nach unten (CncState)
 
 
-            System.out.println(newSvg);
+                    if (s.equals("L") || s.equals("Q")) {
+                        // L = Linear im Material fräsen
+                        // Q = Rundung im Material fräsen
+                        // --> Abstände sind so gering dass auch Linear gefräst werden kann
+                        // Wichtig nur die ersten 2 Werte übernehmen
+
+                        int x = (int) Math.floor(Double.valueOf(svg.get(path)[i + 1]) * millimeterPixelRatio * 1000);
+                        int y = (int) Math.floor(Double.valueOf(svg.get(path)[i + 2]) * millimeterPixelRatio * 1000);
+                        int z = -1;
+
+                        // Koordinate in gCode einfügen
+                        gCode.add(new int[]{x, y, z});
+                    } else if (s.equals("M")) {
+                        // Bewege den Fräskopf an einen neuen Punkt
+                        int x = -1;
+                        int y = -1;
+                        int z = -2;
+                        gCode.add(new int[]{x, y, z});
+
+                        x = (int) Math.floor(Double.valueOf(svg.get(path)[i + 1]) * millimeterPixelRatio * 1000);
+                        y = (int) Math.floor(Double.valueOf(svg.get(path)[i + 2]) * millimeterPixelRatio * 1000);
+                        z = -3;
+                        gCode.add(new int[]{x, y, z});
+                    } else {
+                        // Fehlermeldung + Einfach ignorieren
+                        System.out.println("Unbekannter Befehl: " + s);
+                    }
+
+                    // Neuen Index herausfinden
+
+                    int j = 2;
+
+                    if((i+j) >= svg.get(path).length){
+                        break;
+                    }
+
+                    while (!isString(svg.get(path)[i + j]) && (i + j < svg.get(path).length)) {
+                        j++;
+                        if(i+j >= svg.get(path).length){
+                            break;
+                        }
+                    }
+
+                    i = i + j;      // Index auf den neuen String (nächsten Befehl) setzen
+                    if(i >= svg.get(path).length){
+                        break;
+                    }
+
+                }
+
+            }
+
+            //System.out.println(newSvg);
+
+            for (int[] i : gCode) {
+                System.out.println("x: " + i[0] + "\ty: " + i[1] + "\tz: " + i[2]);
+            }
 
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+
+    private boolean isString(String s) {
+        try {
+            double d = Double.valueOf(s);
+            return false;
+        } catch (Exception e) {
+            // Egal
+        }
+        return true;
     }
 
     public void resizeCoordinates(File svg) {
